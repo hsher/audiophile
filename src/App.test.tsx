7c52ff4f2f5d4e5d411c627460e4c3f14cfda5bd
Yurii Mochalov <ym@anadeainc.com>
2022-09-05 20:29:38 +0300

    fail 3

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,9 @@
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 
 import App from "../pages/index";
 
 test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/test1/i);
 
-  expect(linkElement).toBeInTheDocument();
+  expect(true).toBe(false);
 });
