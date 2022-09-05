0d2f2b77b42283adf65e51ecf96074cd607c440b
Yurii Mochalov <ym@anadeainc.com>
2022-09-05 18:30:03 +0300

    Add failed test for CI check

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,10 @@
 import { render, screen } from "@testing-library/react";
 
-import App from "../pages/index.tsx";
+import App from "../pages/index";
 
 test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/learn react1/i);
 
   expect(linkElement).toBeInTheDocument();
 });
