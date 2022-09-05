d2b54ef2d371c65eb60125da8c18a163837a0b0b
Yurii Mochalov <ym@anadeainc.com>
2022-09-05 16:50:26 +0300

    Export all constants from one file

---
+++ src/design-system/constants/index.tsx
@@ -0,0 +1,8 @@
+import buttonActionElements from "./buttonActionElements";
+import buttonActionThemes from "./buttonActionThemes";
+import { maxMobileScreen, maxTabletScreen } from "./screenSizes";
+
+export { buttonActionElements };
+export { buttonActionThemes };
+export { maxTabletScreen };
+export { maxMobileScreen };
