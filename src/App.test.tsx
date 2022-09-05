5363f842099dc2ba73b5b22b9188c5842d5f4c1a
Yurii Mochalov <ym@anadeainc.com>
2022-09-05 19:54:58 +0300

    Make test fail

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,7 +4,7 @@ import App from "../pages/index";
 
 test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/learn react1/i);
 
   expect(linkElement).toBeInTheDocument();
 });
