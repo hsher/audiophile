f3ab6fd08dbfae870969fcb1f3a021096c5c1cca
Yurii Mochalov <ym@anadeainc.com>
2022-09-12 21:48:36 +0300

    Add Container component

---
+++ src/components/Container/Container.tsx
@@ -0,0 +1,7 @@
+import * as S from "./styled";
+
+function Container({ children }) {
+  return <S.Container>{children}</S.Container>;
+}
+
+export default Container;
---
+++ src/components/Container/styled.tsx
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  max-width: 1190px;
+  padding: 0 40px;
+`;
