2d0d7739fe052f9725fa601fb38ad3682fd6c616
Yurii Mochalov <ym@anadeainc.com>
2022-09-14 20:58:15 +0300

    Add media query mixin

--- src/design-system/constants/index.tsx
+++ src/design-system/constants/index.tsx
@@ -1,10 +1,11 @@
 import actionElements from "./actionElements";
 import { baseFont } from "./baseFont";
 import buttonActionThemes from "./buttonActionThemes";
-import { maxMobileScreen, maxTabletScreen } from "./screenSizes";
+import { device, maxMobileScreen, maxTabletScreen } from "./screenSizes";
 
 export { actionElements };
 export { baseFont };
+export { device };
 export { buttonActionThemes };
 export { maxTabletScreen };
 export { maxMobileScreen };
