a92c4e2e4ba4b75f36de3318d865b33a1db2f3d8
Yurii Mochalov <ym@anadeainc.com>
2022-09-14 21:40:03 +0300

    Update Header according to tablet mockup

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,63 +3,67 @@ import Link from "next/link";
 import cart from "public/cart.svg";
 import logo from "public/logo.svg";
 import menu from "public/menu.svg";
-import ContainerWrapper from "src/components/ContainerWrapper";
+import { useState } from "react";
 import { DARK } from "src/design-system/colors";
 
 import * as S from "./styled";
 
 function Header() {
+  const [menuIsOpen, setMenuIsOpen] = useState(false);
+
+  const handleToggleMenu = () => {
+    setMenuIsOpen(!menuIsOpen);
+  };
+
   return (
-    <ContainerWrapper backgroundColor={DARK} as="header">
+    <S.HeaderWrapper backgroundColor={DARK} as="header">
       <S.ContainerRow>
-        <S.Menu type="button">
+        <S.Menu type="button" onClick={handleToggleMenu}>
           <Image src={menu} alt="open navigation" width={16} height={15} />
         </S.Menu>
 
-        <S.FullWidthColumn>
+        <S.LogoWrapper>
           <Link href="/">
             <a>
               <Image src={logo} alt="audiophile logo" width={143} height={25} />
             </a>
           </Link>
-        </S.FullWidthColumn>
+        </S.LogoWrapper>
 
-        <S.FullWidthColumn>
-          <nav>
-            <S.NavList>
-              <li>
-                <Link href="/" passHref>
-                  <S.NavLink>HOME</S.NavLink>
-                </Link>
-              </li>
-              <li>
-                <Link href="/" passHref>
-                  <S.NavLink>HEADPHONES</S.NavLink>
-                </Link>
-              </li>
-              <li>
-                <Link href="/" passHref>
-                  <S.NavLink>SPEAKERS</S.NavLink>
-                </Link>
-              </li>
-              <li>
-                <Link href="/" passHref>
-                  <S.NavLink>EARPHONES</S.NavLink>
-                </Link>
-              </li>
-            </S.NavList>
-          </nav>
-        </S.FullWidthColumn>
+        <S.Nav menuIsOpen={menuIsOpen}>
+          <S.NavList>
+            <S.NavListItem>
+              <Link href="/" passHref>
+                <S.NavLink>HOME</S.NavLink>
+              </Link>
+            </S.NavListItem>
+            <S.NavListItem>
+              <Link href="/" passHref>
+                <S.NavLink>HEADPHONES</S.NavLink>
+              </Link>
+            </S.NavListItem>
+            <S.NavListItem>
+              <Link href="/" passHref>
+                <S.NavLink>SPEAKERS</S.NavLink>
+              </Link>
+            </S.NavListItem>
+            <S.NavListItem>
+              <Link href="/" passHref>
+                <S.NavLink>EARPHONES</S.NavLink>
+              </Link>
+            </S.NavListItem>
+          </S.NavList>
+        </S.Nav>
 
-        <S.FullWidthColumn align="end">
+        <S.CartWrapper>
           <Link href="/">
             <a>
               <Image src={cart} alt="cart" width={24} height={20} />
             </a>
           </Link>
-        </S.FullWidthColumn>
+        </S.CartWrapper>
       </S.ContainerRow>
-    </ContainerWrapper>
+    </S.HeaderWrapper>
   );
 }
 
--- src/components/Header/styled.tsx
+++ src/components/Header/styled.tsx
@@ -1,5 +1,7 @@
 import Container from "src/components/Container";
-import { GRAY_LIGHT, OCHRE, WHITE } from "src/design-system/colors";
+import ContainerWrapper from "src/components/ContainerWrapper";
+import { DARK, GRAY_LIGHT, OCHRE, WHITE } from "src/design-system/colors";
+import { device } from "src/design-system/constants";
 import styled from "styled-components";
 
 export const NavLink = styled.a`
@@ -12,8 +14,20 @@ export const NavLink = styled.a`
   padding: 0 16px;
   text-decoration: none;
 
+  @media ${device.tablet} {
+    align-items: center;
+    display: flex;
+  }
+
   &:hover {
     color: ${OCHRE};
+  }
+`;
+
+export const HeaderWrapper = styled(ContainerWrapper)`
+  @media ${device.tablet} {
+    padding: 0 40px;
+    position: relative;
   }
 `;
 
@@ -22,6 +36,7 @@ export const ContainerRow = styled(Container)`
   border-bottom: 1px solid ${GRAY_LIGHT}38;
   display: flex;
   justify-content: space-between;
+  min-height: 90px;
 `;
 
 export const Menu = styled.button`
@@ -29,15 +44,54 @@ export const Menu = styled.button`
   border: 0;
   display: none;
   padding: 0;
+
+  @media ${device.tablet} {
+    display: block;
+  }
 `;
 
 export const NavList = styled.ul`
   display: flex;
   padding: 32px 0 36px;
+
+  @media ${device.tablet} {
+    background-color: ${DARK};
+    flex-direction: column;
+    padding: 32px 40px 36px;
+    position: absolute;
+    width: 100%;
+  }
 `;
 
-export const FullWidthColumn = styled.div`
-  display: ${(props) => (props.align === "end" ? "flex" : "block")};
+export const CartWrapper = styled.div`
+  display: flex;
   flex: 1;
-  justify-content: ${(props) => (props.align === "end" ? "flex-end" : "")};
+  justify-content: flex-end;
+`;
+
+export const LogoWrapper = styled.div`
+  flex: 1;
+
+  @media ${device.tablet} {
+    padding-left: 42px;
+  }
+`;
+
+export const Nav = styled.nav`
+  flex: 1;
+
+  @media ${device.tablet} {
+    bottom: 0;
+    display: ${(props) => (props.menuIsOpen ? "block" : "none")};
+    left: 0;
+    position: absolute;
+    width: 100%;
+  }
+`;
+
+export const NavListItem = styled.li`
+  @media ${device.tablet} {
+    border-bottom: 1px solid ${GRAY_LIGHT}38;
+    padding: 5px 0;
+  }
 `;
