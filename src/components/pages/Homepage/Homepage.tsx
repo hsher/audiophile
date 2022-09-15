211882432ca8df4659bf2bf2785053acf8ccfe70
Yurii Mochalov <ym@anadeainc.com>
2022-09-15 17:07:51 +0300

    Prepare Homepage for Hero section

--- src/components/pages/Homepage/Homepage.tsx
+++ src/components/pages/Homepage/Homepage.tsx
@@ -1,5 +1,13 @@
+import Container from "src/components/Container";
+import ContainerWrapper from "src/components/ContainerWrapper";
+import { DARK } from "src/design-system/colors";
+
 function Homepage() {
-  return <div />;
+  return (
+    <ContainerWrapper backgroundColor={DARK}>
+      <Container>Hero Section</Container>
+    </ContainerWrapper>
+  );
 }
 
 export default Homepage;
