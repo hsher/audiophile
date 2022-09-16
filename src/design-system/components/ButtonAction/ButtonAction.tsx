16dab71972d9736619a1353bc8ac93a623d9e1fd
Yurii Mochalov <ym@anadeainc.com>
2022-09-16 15:47:21 +0300

    Use typescript props definition

--- src/design-system/components/ButtonAction/ButtonAction.tsx
+++ src/design-system/components/ButtonAction/ButtonAction.tsx
@@ -1,9 +1,10 @@
-import { func, node, oneOf } from "prop-types";
 import { actionElements, buttonActionThemes } from "src/design-system/constants";
 
 import * as S from "./styled";
 
-function ButtonAction({ children, element, href, onClick, theme, type, ...passThroughProps }) {
+function ButtonAction(props: ButtonActionProps) {
+  const { children, element, href, onClick, theme, type, ...passThroughProps } = props;
+
   return (
     <S.ButtonActionBase
       as={element}
@@ -18,46 +19,24 @@ function ButtonAction({ children, element, href, onClick, theme, type, ...passTh
   );
 }
 
-ButtonAction.propTypes = {
-  children: node.isRequired,
-  /*
-   * ButtonAction can be configured to render either an HTML anchor element or
-   * and HTML button element.
-   *
-   *  By default, ButtonAction will render a button element.
-   * Valid element types are referenced through the actionElements enum:
-   * "design-system/constants/actionElements".
-   * */
-  element: oneOf(Object.values(actionElements)),
-  /**
-   * When ButtonAction renders an anchor element, href is required
-   * */
-  href: (props) => {
-    if (!props.href && props.element === actionElements.A) {
-      return new Error("The href prop is required when ButtonAction is a link");
-    }
-
-    if (typeof props.href !== "string" && props.element === actionElements.A) {
-      return new Error("href must be a string");
-    }
-
-    return null;
-  },
-  onClick: func,
-  type: oneOf(["button", "submit", "reset"]),
-  /**
-   * Determines background and text color of ButtonAction.
-   * Values for the theme prop are imported from the buttonActionThemes.
-   * */
-  theme: oneOf(Object.values(buttonActionThemes)),
-};
-
 ButtonAction.defaultProps = {
   element: actionElements.BUTTON,
   href: null,
-  onClick: null,
+  onClick: () => null,
   theme: buttonActionThemes.PRIMARY,
-  type: "submit",
+  type: null,
 };
+
+interface ButtonActionProps {
+  children: React.ReactNode;
+  element?: Element;
+  href?: string;
+  onClick?: () => void;
+  theme?: Theme;
+  type?: "button" | "submit" | "reset";
+}
+
+type Element = typeof actionElements[keyof typeof actionElements];
+type Theme = typeof buttonActionThemes[keyof typeof buttonActionThemes];
 
 export default ButtonAction;
